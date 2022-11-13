345a8ae84489ef9cdf4c5ca743b27d2f422f1db7
Anirudh Sharma <76525607+AnirudhJS07@users.noreply.github.com>
2022-11-13 11:33:38 +0530

    Update settings.py

--- LMS/settings.py
+++ LMS/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'pktw^-1fml7^l$tv0xyr$()h=!tn!e%#l$x6#17+xah)xpx&7+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
